1630eb68da89d4c5810c492767a043c568bdcb10
tom darby <51497148+drby@users.noreply.github.com>
2025-02-27 13:31:28 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Box, Heading } from '@chakra-ui/react';
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <Box p={5} bg="red.100" m={5}>
       <Heading>Hello World</Heading>
